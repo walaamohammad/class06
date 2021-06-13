abf08e062b3406cdc59582e6248f0bbcdbb6e6ce
walaamohammad <layan_abadi86@yahoo.com>
2021-06-14 00:22:31 +0300

    add

---
+++ app.js
@@ -0,0 +1,203 @@
+
+
+'use strict';
+let hourse = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+
+
+function randomNum(min, max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+//__________________________ locations______________________________________//
+
+
+
+
+
+let seattle = {
+    name: 'seattle',
+    avg: 6.3,
+    minCus: 23,
+    maxCus: 65,
+    cookies: [],
+    totalSale: 0,
+    getCookies: function () {
+        let total = 0;
+    }
+}
+for (let i = 0; i < hourse.length; i++) {
+    let randomCust = random(this.min, this.max);
+    let salmonCookies = math.ceil(randomCust * this.avg);
+    this.hourse[i] = salmonCookies;
+    total = total + this.hourse[i];
+    this.totalSale = total;
+}
+function salmons() {
+    let salmonCookie = document.createElement('div');
+    let articleElement = document.createElement('article');
+    salmonCookie.appendChild(articleElement);
+    let cities = document.createElement('h2');
+    articleElement.appendChild(cities);
+    cities.textcontent = this.name;
+    let hourseList = document.createElement('ul');
+    articleElement.appendChild(houreslist);
+
+    for (let i = 0; i < houres.length;)i++)
+    {
+        let list = document.createElement('li');
+        houreslist.appendChild(list);
+        list.textContent = `Total: ${this.totalSale} cookies`
+    }
+
+}
+
+let tokyo = {
+    name: 'tokyo',
+    avg: 1.2,
+    minCus: 3,
+    maxCus: 24,
+    cookies: [],
+    totalSale: 0,
+    getCookies: function () {
+        let total = 0;
+    }
+}
+for (let i = 0; i < hourse.length; i++) {
+    let randomCust = random(this.min, this.max);
+    let salmonCookies = math.ceil(randomCust * this.avg);
+    this.hourse[i] = salmonCookies;
+    total = total + this.hourse[i];
+    this.totalSale = total;
+
+    function salmons() {
+        let salmonCookie = document.createElement('div');
+        let articleElement = document.createElement('article');
+        salmonCookie.appendChild(articleElement);
+        let cities = document.createElement('h2');
+        articleElement.appendChild(cities);
+        cities.textcontent = this.name;
+        let hourseList = document.createElement('ul');
+        articleElement.appendChild(houreslist);
+
+        for (let i = 0; i < houres.length;)i++)
+        {
+            let list = document.createElement('li');
+            houreslist.appendChild(list);
+            list.textContent = `Total: ${this.totalSale} cookies`
+        }
+
+    }
+
+    let dubai = {
+        name: 'dubai',
+        avg: 3.7,
+        minCus: 11,
+        maxCus: 38,
+        cookies: [],
+        totalSale: 0,
+        getCookies: function () {
+            let total = 0;
+        }
+    }
+    for (let i = 0; i < hourse.length; i++) {
+        let randomCust = random(this.min, this.max);
+        let salmonCookies = math.ceil(randomCust * this.avg);
+        this.hourse[i] = salmonCookies;
+        total = total + this.hourse[i];
+        this.totalSale = total;
+    }
+    function salmons() {
+        let salmonCookie = document.createElement('div');
+        let articleElement = document.createElement('article');
+        salmonCookie.appendChild(articleElement);
+        let cities = document.createElement('h2');
+        articleElement.appendChild(cities);
+        cities.textcontent = this.name;
+        let hourseList = document.createElement('ul');
+        articleElement.appendChild(houreslist);
+
+        for (let i = 0; i < houres.length;)i++)
+        {
+            let list = document.createElement('li');
+            houreslist.appendChild(list);
+            list.textContent = `Total: ${this.totalSale} cookies`
+        }
+
+    }
+
+    let paris = {
+        name: 'paris',
+        avg: 2.3,
+        minCus: 20,
+        maxCus: 38,
+        cookies: [],
+        totalSale: 0,
+        getCookies: function () {
+            let total = 0;
+        }
+    }
+    for (let i = 0; i < hourse.length; i++) {
+        let randomCust = random(this.min, this.max);
+        let salmonCookies = math.ceil(randomCust * this.avg);
+        this.hourse[i] = salmonCookies;
+        total = total + this.hourse[i];
+        this.totalSale = total;
+    }
+    function salmons() {
+        let salmonCookie = document.createElement('div');
+        let articleElement = document.createElement('article');
+        salmonCookie.appendChild(articleElement);
+        let cities = document.createElement('h2');
+        articleElement.appendChild(cities);
+        cities.textcontent = this.name;
+        let hourseList = document.createElement('ul');
+        articleElement.appendChild(houreslist);
+
+        for (let i = 0; i < houres.length;)i++)
+        {
+            let list = document.createElement('li');
+            houreslist.appendChild(list);
+            list.textContent = `Total: ${this.totalSale} cookies`
+        }
+
+    }
+
+    let lima = {
+        name: 'lima',
+        avg: 2,
+        minCus: 16,
+        maxCus: 46,
+        cookies: [],
+        totalSale: 0,
+        getCookies: function () {
+            let total = 0;
+        }
+    }
+    for (let i = 0; i < hourse.length; i++) {
+        let randomCust = random(this.min, this.max);
+        let salmonCookies = math.ceil(randomCust * this.avg);
+        this.hourse[i] = salmonCookies;
+        total = total + this.hourse[i];
+        this.totalSale = total;
+    }
+    function salmons() {
+        let salmonCookie = document.createElement('div');
+        let articleElement = document.createElement('article');
+        salmonCookie.appendChild(articleElement);
+        let cities = document.createElement('h2');
+        articleElement.appendChild(cities);
+        cities.textcontent = this.name;
+        let hourseList = document.createElement('ul');
+        articleElement.appendChild(houreslist);
+
+        for (let i = 0; i < houres.length;)i++)
+        {
+            let list = document.createElement('li');
+            houreslist.appendChild(list);
+            list.textContent = `Total: ${this.totalSale} cookies`
+        }
+
